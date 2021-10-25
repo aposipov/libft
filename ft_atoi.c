c9bd5c7c1fa95d21bdbac5726332677b5b2a7782
aposipov <a.p.osipov@ro.ru>
2021-10-25 16:27:34 +1000

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lchristi <lchristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 17:20:38 by lchristi          #+#    #+#             */
-/*   Updated: 2021/10/25 10:34:55 by lchristi         ###   ########.fr       */
+/*   Updated: 2021/10/25 16:25:42 by lchristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int	i;
-	int	minus;
-	int	num;
+	int			i;
+	int			sign;
+	long int	old;
+	long int	num;
 
 	i = 0;
-	minus = 1;
+	sign = 1;
 	num = 0;
-	if (!nptr)
-		return (0);
 	while (nptr[i] == '\t' || nptr[i] == '\n' || nptr[i] == '\v' \
 					|| nptr[i] == '\f' || nptr[i] == '\r' || nptr[i] == ' ')
 		i++;
 	if (nptr[i] == '-')
-		minus = -1;
+		sign = -1;
 	if (nptr[i] == '-' || nptr[i] == '+')
 		i++;
 	while (nptr[i] >= '0' && nptr[i] <= '9')
-		num = (num * 10) + (nptr[i++] - '0');
-	return (num * minus);
+	{	
+		old = num;
+		num = num * 10 + (nptr[i++] - '0');
+		if ((old < 0 && num > 0) || (old > 0 && num < 0))
+			return ((sign == 1) * -1);
+	}
+	return (num * sign);
 }
---
+++ libft-mytests/test-atoi.c
@@ -0,0 +1,10 @@
+#include "../libft.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("%d\n", ft_atoi("214748365045454565"));
+	printf("%d\n", atoi("214748365045454565"));	
+	printf("%d\n", ft_atoi(""));
+	printf("%d\n", atoi(""));	
+}
